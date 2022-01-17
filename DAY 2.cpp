3549c6e52213f4463e068f59fbfa357fff5b6d26
Nirbhik16 <87673341+Nirbhik16@users.noreply.github.com>
2022-01-17 22:05:10 +0530

    Update DAY 2.cpp

--- DAY 2.cpp
+++ DAY 2.cpp
@@ -1,1 +1,36 @@
+/*Question 1= 977. Squares of a Sorted Array*/
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
+
+
+/*Question 2= 189. Rotate Array*/
+
+class Solution {
+public:
+    vector<int> reverse(vector<int>& nums,int start,int end){
+        while(start<=end){
+            swap(nums[start],nums[end]);
+            start++;
+            end--;
+        }
+        return nums;
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+};
 
